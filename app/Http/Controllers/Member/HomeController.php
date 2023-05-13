222722041364b2ed61e55c7d1d2d3bd2e16d9cde
hoyirul <irulgraspama621@gmail.com>
2023-05-13 15:52:52 +0700

    Fixed banner and book list

--- app/Http/Controllers/Member/HomeController.php
+++ app/Http/Controllers/Member/HomeController.php
@@ -4,14 +4,30 @@ namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
 use App\Models\Banner;
+use App\Models\Book;
+use App\Models\BookRating;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
         $banners = Banner::all();
+        $books = Book::all();
+        $descbooks = Book::orderBy('id', 'DESC')->get();
         return view('member.home.index', compact([
-            'banners'
+            'banners',
+            'books',
+            'descbooks',
         ]));
+    }
+
+    public static function rating($id){
+        $avg = BookRating::where('book_id', $id)->avg('star');
+        return ($avg == null) ? 0 : $avg;
+    }
+
+    public static function reviews_from_rating($id){
+        $count = BookRating::where('book_id', $id)->count('book_id');
+        return ($count == null) ? 0 : $count;
     }
 }
--- resources/views/member/home/index.blade.php
+++ resources/views/member/home/index.blade.php
@@ -19,12 +19,10 @@
       </div>
       <!-- Slider indicators -->
       <div class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1"
+        @foreach ($banners as $item)
+          <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="{{ 'Slide '.$item->numbers }}"
           data-carousel-slide-to="0"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
-          data-carousel-slide-to="1"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3"
-          data-carousel-slide-to="2"></button>
+        @endforeach
       </div>
       <!-- Slider controls -->
       <button type="button"
@@ -56,118 +54,62 @@
 </div>
 
 <div class="mt-8 px-4 flex justify-between">
-  <div class="py-4 bg-gray-400 w-40 rounded-md"></div>
+  <h1 class="text-gray-600 montserrat-semibold text-2xl">Buku Terbaru</h1>
   <a href="/book.html" class="w-48 py-2 bg-green-600 rounded-lg text-white montserrat-medium text-sm cursor-pointer">Lihat Semua
   </a>
 </div>
 
 <div class="grid grid-cols-8 gap-4 mt-2">
 
-  <div class="col-span-6 sm:col-span-4 md:col-span-4 lg:col-span-2 xl:col-span-2">
-    <div class="bg-white shadow-lg rounded-lg px-4 py-6 mx-4 my-4">
-      <div class="mx-auto h-40 bg-gray-200 rounded-md"></div>
-      <div class="h-4 bg-gray-300 w-40 mt-8 block mx-auto rounded-sm"></div>
-      <div class="h-2 bg-gray-200 w-64 md:w-20 sm:w-20 mt-2 block mx-auto rounded-sm"></div>
-      <div class="flex justify-center mt-4">
-        <div class="rounded-sm h-8 w-20 px-4 bg-gray-200 mr-2"></div>
-        <div class="rounded-sm h-8 w-20 px-4 bg-green-400"></div>
+  @foreach ($books as $item)
+    <div class="col-span-6 sm:col-span-4 md:col-span-4 lg:col-span-2 xl:col-span-2">
+      <div class="bg-white shadow-lg rounded-lg px-4 py-6 mx-4 my-4">
+        <div class="overflow-hidden mx-auto h-64 bg-gray-200 rounded-md">
+          <img src="{{ asset('storage/'.$item->book_cover) }}" alt="">
+        </div>
+        <h3 class="mt-8 montserrat-semibold text-gray-600">{{ $item->title }}</h3>
+        <p class="mt-2">{{ $item->author.' | '.$item->publisher }}</p>
+        <div class="flex justify-center mt-4">
+          <div class="flex items-center">
+            <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <p class="ml-2 text-sm font-bold text-gray-900 dark:text-gray-500">{{ \App\Http\Controllers\Member\HomeController::rating($item->id) }}</p>
+            <span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
+            <a href="#" class="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-gray-500">{{ \App\Http\Controllers\Member\HomeController::reviews_from_rating($item->id) }} reviews</a>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-
-  <div class="col-span-6 sm:col-span-4 md:col-span-4 lg:col-span-2 xl:col-span-2">
-    <div class="bg-white shadow-lg rounded-lg px-4 py-6 mx-4 my-4">
-      <div class="mx-auto h-40 bg-gray-200 rounded-md"></div>
-      <div class="h-4 bg-gray-300 w-40 mt-8 block mx-auto rounded-sm"></div>
-      <div class="h-2 bg-gray-200 w-64 md:w-20 sm:w-20 mt-2 block mx-auto rounded-sm"></div>
-      <div class="flex justify-center mt-4">
-        <div class="rounded-sm h-8 w-20 px-4 bg-gray-200 mr-2"></div>
-        <div class="rounded-sm h-8 w-20 px-4 bg-green-400"></div>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-span-6 sm:col-span-4 md:col-span-4 lg:col-span-2 xl:col-span-2">
-    <div class="bg-white shadow-lg rounded-lg px-4 py-6 mx-4 my-4">
-      <div class="mx-auto h-40 bg-gray-200 rounded-md"></div>
-      <div class="h-4 bg-gray-300 w-40 mt-8 block mx-auto rounded-sm"></div>
-      <div class="h-2 bg-gray-200 w-64 md:w-20 sm:w-20 mt-2 block mx-auto rounded-sm"></div>
-      <div class="flex justify-center mt-4">
-        <div class="rounded-sm h-8 w-20 px-4 bg-gray-200 mr-2"></div>
-        <div class="rounded-sm h-8 w-20 px-4 bg-green-400"></div>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-span-6 sm:col-span-4 md:col-span-4 lg:col-span-2 xl:col-span-2">
-    <div class="bg-white shadow-lg rounded-lg px-4 py-6 mx-4 my-4">
-      <div class="mx-auto h-40 bg-gray-200 rounded-md"></div>
-      <div class="h-4 bg-gray-300 w-40 mt-8 block mx-auto rounded-sm"></div>
-      <div class="h-2 bg-gray-200 w-64 md:w-20 sm:w-20 mt-2 block mx-auto rounded-sm"></div>
-      <div class="flex justify-center mt-4">
-        <div class="rounded-sm h-8 w-20 px-4 bg-gray-200 mr-2"></div>
-        <div class="rounded-sm h-8 w-20 px-4 bg-green-400"></div>
-      </div>
-    </div>
-  </div>
+  @endforeach
 
 </div>
 
 <div class="mt-8 px-4 flex justify-between">
-  <div class="py-4 bg-gray-400 w-40 rounded-md"></div>
+  <h1 class="text-gray-600 montserrat-semibold text-2xl">Buku Bisnis</h1>
   <a href="/book.html" class="w-48 py-2 bg-green-600 rounded-lg text-white montserrat-medium text-sm cursor-pointer">Lihat Semua
   </a>
 </div>
 
 <div class="grid grid-cols-8 gap-4 mt-2">
 
-  <div class="col-span-6 sm:col-span-4 md:col-span-4 lg:col-span-2 xl:col-span-2">
-    <div class="bg-white shadow-lg rounded-lg px-4 py-6 mx-4 my-4">
-      <div class="mx-auto h-40 bg-gray-200 rounded-md"></div>
-      <div class="h-4 bg-gray-300 w-40 mt-8 block mx-auto rounded-sm"></div>
-      <div class="h-2 bg-gray-200 w-64 md:w-20 sm:w-20 mt-2 block mx-auto rounded-sm"></div>
-      <div class="flex justify-center mt-4">
-        <div class="rounded-sm h-8 w-20 px-4 bg-gray-200 mr-2"></div>
-        <div class="rounded-sm h-8 w-20 px-4 bg-green-400"></div>
+  @foreach ($descbooks as $item)
+    <div class="col-span-6 sm:col-span-4 md:col-span-4 lg:col-span-2 xl:col-span-2">
+      <div class="bg-white shadow-lg rounded-lg px-4 py-6 mx-4 my-4">
+        <div class="overflow-hidden mx-auto h-64 bg-gray-200 rounded-md">
+          <img src="{{ asset('storage/'.$item->book_cover) }}" alt="">
+        </div>
+        <h3 class="mt-8 montserrat-semibold text-gray-600">{{ $item->title }}</h3>
+        <p class="mt-2">{{ $item->author.' | '.$item->publisher }}</p>
+        <div class="flex justify-center mt-4">
+          <div class="flex items-center">
+            <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <p class="ml-2 text-sm font-bold text-gray-900 dark:text-gray-500">{{ \App\Http\Controllers\Member\HomeController::rating($item->id) }}</p>
+            <span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
+            <a href="#" class="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-gray-500">{{ \App\Http\Controllers\Member\HomeController::reviews_from_rating($item->id) }} reviews</a>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-
-  <div class="col-span-6 sm:col-span-4 md:col-span-4 lg:col-span-2 xl:col-span-2">
-    <div class="bg-white shadow-lg rounded-lg px-4 py-6 mx-4 my-4">
-      <div class="mx-auto h-40 bg-gray-200 rounded-md"></div>
-      <div class="h-4 bg-gray-300 w-40 mt-8 block mx-auto rounded-sm"></div>
-      <div class="h-2 bg-gray-200 w-64 md:w-20 sm:w-20 mt-2 block mx-auto rounded-sm"></div>
-      <div class="flex justify-center mt-4">
-        <div class="rounded-sm h-8 w-20 px-4 bg-gray-200 mr-2"></div>
-        <div class="rounded-sm h-8 w-20 px-4 bg-green-400"></div>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-span-6 sm:col-span-4 md:col-span-4 lg:col-span-2 xl:col-span-2">
-    <div class="bg-white shadow-lg rounded-lg px-4 py-6 mx-4 my-4">
-      <div class="mx-auto h-40 bg-gray-200 rounded-md"></div>
-      <div class="h-4 bg-gray-300 w-40 mt-8 block mx-auto rounded-sm"></div>
-      <div class="h-2 bg-gray-200 w-64 md:w-20 sm:w-20 mt-2 block mx-auto rounded-sm"></div>
-      <div class="flex justify-center mt-4">
-        <div class="rounded-sm h-8 w-20 px-4 bg-gray-200 mr-2"></div>
-        <div class="rounded-sm h-8 w-20 px-4 bg-green-400"></div>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-span-6 sm:col-span-4 md:col-span-4 lg:col-span-2 xl:col-span-2">
-    <div class="bg-white shadow-lg rounded-lg px-4 py-6 mx-4 my-4">
-      <div class="mx-auto h-40 bg-gray-200 rounded-md"></div>
-      <div class="h-4 bg-gray-300 w-40 mt-8 block mx-auto rounded-sm"></div>
-      <div class="h-2 bg-gray-200 w-64 md:w-20 sm:w-20 mt-2 block mx-auto rounded-sm"></div>
-      <div class="flex justify-center mt-4">
-        <div class="rounded-sm h-8 w-20 px-4 bg-gray-200 mr-2"></div>
-        <div class="rounded-sm h-8 w-20 px-4 bg-green-400"></div>
-      </div>
-    </div>
-  </div>
+  @endforeach
 
 </div>
 
