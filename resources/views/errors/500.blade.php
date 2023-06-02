7bddc6d9e1cdb87beb0ba4daa4e480d3a0d74812
hoyirul <irulgraspama621@gmail.com>
2023-06-02 20:27:23 +0700

    Add errors

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Oops</title>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <style>
+    #main {
+        height: 100vh;
+    }
+  </style>
+</head>
+<body>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="d-flex justify-content-center align-items-center" id="main">
+    <h1 class="mr-3 pr-3 align-top border-right inline-block align-content-center">Oops</h1>
+    <div class="inline-block align-middle">
+    	<h2 class="font-weight-normal lead" id="desc">Something went wrong.</h2>
+    </div>
+</div>
+</body>
+</html>
